faebb461b18a9a3c8bce22f289f1b558ce2a65d5
lsprz2773 <243757@ids.upchiapas.edu.mx>
2026-04-22 10:40:06 -0600

    styles for avatar component

--- src/app/components/ui/Avatar.tsx
+++ src/app/components/ui/Avatar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Avatar = ({ name, size = 36, color }) => {
+const Avatar = ({ name, size = 36, color = "#60a5fa" }) => {
     const initial = (name ?? "?")
         .split(" ")
         .map((w) => w[0])
@@ -9,7 +9,16 @@ const Avatar = ({ name, size = 36, color }) => {
         .toUpperCase();
 
     return (
-        <div>
+        <div
+            className="rounded-full flex items-center justify-center shrink-0 font-extrabold"
+            style={{
+                width: size,
+                height: size,
+                background: color + "22",
+                fontSize: size * 0.36,
+                color,
+            }}
+        >
             {initial}
         </div>
     );
